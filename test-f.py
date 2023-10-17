c1661dcf3b13880bf00bfd8940a8d8620e1ce3e8
Ruth <131242482+ruth8607253@users.noreply.githhub.com>
2023-10-17 08:37:37 +0800

    筆記

--- test-f.py
+++ test-f.py
@@ -4,9 +4,15 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template('test-f.html')
 
-@app.route("/test")
-def hellohello():
-    return "<h1>bbbb</h1>"
+def hello(name):
+    return f"Hello, {name}."
+
+@app.route("/<name>")
+def hellohello(name):
+    data={
+        "name":name,
+        "age":18,
+        "location":"Taiwan",
+    }
+    return render_template('test-f.html',**locals())
